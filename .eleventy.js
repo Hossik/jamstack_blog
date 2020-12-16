f44f84bb745a002f4d3d0afe5654481689ca8e7f
Hossik <hossik1983@gmail.com>
2020-12-16 14:24:43 +0200

    initial

--- .eleventy.js
+++ .eleventy.js
@@ -8,5 +8,12 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter('dump', obj =>{
     return util.inspect(obj);
-  })
+  });
+
+  eleventyConfig.addFilter('formatDate', value => {
+    const ISOcode = 'en-GB';
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const d = new Date(value);
+    return `${d.toLocaleDateString(ISOcode, options)}`;
+   })
   };
